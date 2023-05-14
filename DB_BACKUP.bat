b7242fc01a9c3e717299adec209bff84e8da47d8
VitalijsMuzalovs <102912469+VitalijsMuzalovs@users.noreply.github.com>
2023-05-14 20:46:16 +0300

    upd_backup

---
+++ DB_BACKUP.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+COPY /v/y/z E:\PROJECTS\Kristal\DB_Kristal.accdb E:\PROJECTS\Kristal\Backup\DB_Kristal.accdb
+COPY /v/y/z E:\PROJECTS\Kristal\Kristal.accdr E:\PROJECTS\Kristal\Backup\Kristal.accdr
+PAUSE
